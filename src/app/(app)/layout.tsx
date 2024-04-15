a647075cc95d1895f70bd58b37da0a0c27f9e3fb
wmhafiz <fizyboy@gmail.com>
2024-04-15 22:53:52 +0800

    update page

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -268,7 +268,7 @@ export default function RootLayout({
             <SideNav />
             <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
               <TopNav />
-              <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+              <main>
                 {children}
               </main>
             </div>
--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,25 +1,66 @@
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
 import { Button } from "@/components/ui/button"
 import {
   Card,
+  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 
-export default function Home() {
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  })
+
+  const data = await payload.find({
+    collection: 'diaries',
+  })
+
+  const totalCal = data.docs.reduce((prev, curr) => prev += curr.total_calories || 0, 0)
+
   return (
-    <Card className="sm:col-span-2">
-      <CardHeader className="pb-3">
-        <CardTitle>Your Orders</CardTitle>
-        <CardDescription className="max-w-lg text-balance leading-relaxed">
-          Introducing Our Dynamic Orders Dashboard for Seamless Management and
-          Insightful Analysis.
-        </CardDescription>
-      </CardHeader>
-      <CardFooter>
-        <Button>Create New Order</Button>
-      </CardFooter>
-    </Card>
+    <div className="grid grid-cols-3 gap-4 p-4">
+      <Card>
+        <CardHeader className="pb-3">
+          <CardTitle>Your Diaries</CardTitle>
+          <CardDescription className="max-w-lg text-balance leading-relaxed">
+            Introducing Our Dynamic Orders Dashboard for Seamless Management and
+            Insightful Analysis.
+          </CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <Button>Create New Diary</Button>
+        </CardFooter>
+      </Card>
+      <Card>
+        <CardHeader className="pb-2">
+          <CardDescription>This Week</CardDescription>
+          <CardTitle className="text-4xl">{totalCal}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="text-xs text-muted-foreground">+25% from last week</div>
+        </CardContent>
+        <CardFooter>
+          {/* <Progress value={25} aria-label="25% increase" /> */}
+        </CardFooter>
+      </Card>
+      <Card>
+        <CardHeader className="pb-2">
+          <CardDescription>This Month</CardDescription>
+          <CardTitle className="text-4xl">$5,329</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="text-xs text-muted-foreground">
+            +10% from last month
+          </div>
+        </CardContent>
+        <CardFooter>
+          {/* <Progress value={12} aria-label="12% increase" /> */}
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
